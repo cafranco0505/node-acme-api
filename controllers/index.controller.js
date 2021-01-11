ca397d69184c5742acc1e79a9088b7ac322ba15e
cafranco0505 <48168598+cafranco0505@users.noreply.github.com>
2021-01-10 23:15:27 -0500

    update api

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -9,12 +9,66 @@ const pool = new Pool({
 });
 
 const getEmp = async (req, res) => {
-    const response = await pool.query('SELECT * FROM asesores ORDER BY nombre ASC');
+    const response = await pool.query('SELECT * FROM asesores INNER JOIN niveles ON asesores.nivel = niveles.id_nivel ORDER BY id_asesor ASC');
     res.status(200).json(response.rows);
 };
+
+const getSales = async (req, res) => {
+    const response = await pool.query('SELECT * FROM ventas');
+    res.status(200).json(response.rows);
+};
+
+const getExp = async (req, res) => {
+    const response = await pool.query('SELECT * FROM niveles');
+    res.status(200).json(response.rows);
+};
+
+const getPayments = async (req, res) => {
+    const response = await pool.query('SELECT * FROM pagos');
+    res.status(200).json(response.rows);
+};
+
+const createEmp = async (req, res) => {
+    try {
+        // peticion asincrona
+        const { nombre, apellido1, apellido2, direccion, telefono, email, nivel } = req.body;
+        const response = await pool.query('INSERT INTO asesores(nombre, apellido1, apellido2, direccion , telefono , email , nivel) values($1, $2, $3, $4, $5, $6, $7)', [nombre, apellido1, apellido2, direccion, telefono, email, nivel]);
+        res.json({
+            message: 'Employee Created',
+            body: {
+                user: { nombre, apellido1, apellido2, direccion, telefono, email, nivel }
+            }
+        });
+    }
+    catch (e) {
+        console.log(e);
+        return res.status(500).json('Internal Server error');
+    }
+};
+
+
+const deleteEmp = async (req, res) => {
+    const id = parseInt(req.params.id);
+    await pool.query('DELETE FROM asesores where id_asesor = $1', [id]);
+    res.json(`Empleado ${id} borrado`);
+};
+// const getEmp = async (req, res) => {
+//     const response = await pool.query('SELECT * FROM asesores INNER JOIN niveles ON asesores.nivel = niveles.id_nivel ORDER BY id_asesor ASC');
+//     res.status(200).json(response.rows);
+// };
+
+// const getEmp = async (req, res) => {
+//     const response = await pool.query('SELECT * FROM asesores INNER JOIN niveles ON asesores.nivel = niveles.id_nivel ORDER BY id_asesor ASC');
+//     res.status(200).json(response.rows);
+// };
 
 
 
 module.exports = {
-    getEmp
+    getEmp,
+    getSales,
+    getExp,
+    getPayments,
+    createEmp,
+    deleteEmp
 };
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,13 @@ const router = Router();
 //     res.send('employees');
 // });
 
-const { getEmp, getUserById, createUser, updateUser, deleteUser } = require('../controllers/index.controller');
+const { getEmp, getSales, getExp, getPayments, createEmp, deleteEmp} = require('../controllers/index.controller');
 
 router.get('/api/employees', getEmp);
+router.get('/api/sales', getSales);
+router.get('/api/exp', getExp);
+router.get('/api/payments', getPayments);
+router.post('/api/employees', createEmp);
+router.delete('/api/employees/:id', deleteEmp);
 
 module.exports = router;
