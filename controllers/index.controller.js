bb1cf01f7793a89a49c211fb0891e79c6d4ed9a8
cafranco0505 <48168598+cafranco0505@users.noreply.github.com>
2021-01-11 14:12:58 -0500

    sales and payments request

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -7,27 +7,20 @@ const pool = new Pool({
     database: 'empleados_acme',
     port: '5432'
 });
-
+// get all employees and exp lvel
 const getEmp = async (req, res) => {
     const response = await pool.query('SELECT * FROM asesores INNER JOIN niveles ON asesores.nivel = niveles.id_nivel ORDER BY id_asesor ASC');
     res.status(200).json(response.rows);
 };
 
-const getSales = async (req, res) => {
-    const response = await pool.query('SELECT * FROM ventas');
-    res.status(200).json(response.rows);
+// get employee by id
+const getEmpById = async (req, res) => {
+    const id = parseInt(req.params.id);
+    await pool.query('SELECT * FROM asesores where id_asesor = $1', [id]);
+    res.json(`Empleado ${id} borrado`);
 };
 
-const getExp = async (req, res) => {
-    const response = await pool.query('SELECT * FROM niveles');
-    res.status(200).json(response.rows);
-};
-
-const getPayments = async (req, res) => {
-    const response = await pool.query('SELECT * FROM pagos');
-    res.status(200).json(response.rows);
-};
-
+// create employee
 const createEmp = async (req, res) => {
     try {
         // peticion asincrona
@@ -46,29 +39,96 @@ const createEmp = async (req, res) => {
     }
 };
 
-
+// delete employee by id
 const deleteEmp = async (req, res) => {
     const id = parseInt(req.params.id);
     await pool.query('DELETE FROM asesores where id_asesor = $1', [id]);
     res.json(`Empleado ${id} borrado`);
 };
-// const getEmp = async (req, res) => {
-//     const response = await pool.query('SELECT * FROM asesores INNER JOIN niveles ON asesores.nivel = niveles.id_nivel ORDER BY id_asesor ASC');
-//     res.status(200).json(response.rows);
-// };
 
-// const getEmp = async (req, res) => {
-//     const response = await pool.query('SELECT * FROM asesores INNER JOIN niveles ON asesores.nivel = niveles.id_nivel ORDER BY id_asesor ASC');
-//     res.status(200).json(response.rows);
-// };
+// get all sales
+const getSales = async (req, res) => {
+    const response = await pool.query('SELECT * FROM ventas');
+    res.status(200).json(response.rows);
+};
+
+// create Sale
+const createSale = async (req, res) => {
+    try {
+        // peticion asincrona
+        const { valor, asesor} = req.body;
+        const response = await pool.query('INSERT INTO ventas(valor, asesor, fecha_factura) values($1, $2, CURRENT_TIMESTAMP)', [valor, asesor]);
+        res.json({
+            message: 'Sale Created',
+            body: {
+                user: { valor, asesor }
+            }
+        });
+    }
+    catch (e) {
+        console.log(e);
+        return res.status(500).json('Internal Server error');
+    }
+};
+
+// get sales SUM
+const getSalesSUM = async (req, res) => {
+    const response = await pool.query('SELECT SUM(valor) FROM ventas');
+    res.status(200).json(response.rows);
+};
+
+// get employees sales SUM
+const getEmpSalesSUM = async (req, res) => {
+    const id = parseInt(req.params.id);
+    await pool.query('SELECT SUM(valor), asesor FROM ventas GROUP BY asesor HAVING asesor = $1', [id]);
+    res.json(`Empleado ${id} borrado`);
+};
+
+// create pay
+const createPay = async (req, res) => {
+    try {
+        // peticion asincrona
+        const { salud, pension, riesgos, comision, total, asesor} = req.body;
+        const response = await pool.query('INSERT INTO ventas(salud, pension, riesgos, comision, total, asesor, fecha_pago) values($1, $2, $4, $5, $6, CURRENT_TIMESTAMP)', [salud, pension, riesgos, comision, total, asesor]);
+        res.json({
+            message: 'Check',
+            body: {
+                user: { salud, pension, riesgos, comision, total, asesor }
+            }
+        });
+    }
+    catch (e) {
+        console.log(e);
+        return res.status(500).json('Internal Server error');
+    }
+};
+
+
+// get all payments
+const getPayments = async (req, res) => {
+    const response = await pool.query('SELECT * FROM pagos');
+    res.status(200).json(response.rows);
+};
+
+// get payments by Employee
+const getPaymentsById = async (req, res) => {
+    const response = await pool.query('SELECT * FROM pagos');
+    res.status(200).json(response.rows);
+};
+
+
+
 
 
 
 module.exports = {
     getEmp,
-    getSales,
-    getExp,
-    getPayments,
+    getEmpById,
     createEmp,
-    deleteEmp
+    deleteEmp,
+    getSales,
+    createSale,
+    getSalesSUM,
+    getEmpSalesSUM,
+    getPayments
 };
--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,19 @@ const router = Router();
 //     res.send('employees');
 // });
 
-const { getEmp, getSales, getExp, getPayments, createEmp, deleteEmp} = require('../controllers/index.controller');
+const { getEmp, getEmpById, createEmp, deleteEmp, getSales,createSale, getSalesSUM, getEmpSalesSUM} = require('../controllers/index.controller');
 
+// peticiones empleados
 router.get('/api/employees', getEmp);
-router.get('/api/sales', getSales);
-router.get('/api/exp', getExp);
-router.get('/api/payments', getPayments);
+router.get('/api/employees/:id', getEmpById);
 router.post('/api/employees', createEmp);
 router.delete('/api/employees/:id', deleteEmp);
+// peticiones ventas
+router.get('/api/sales', getSales);
+router.post('/api/sales', createSale);
+router.get('/api/sales', getSalesSUM);
+router.get('/api/sales/:id', getEmpSalesSUM);
+// peticiones pagos
+
 
 module.exports = router;
